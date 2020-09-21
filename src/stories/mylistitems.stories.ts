d604de7200b349a329719dd768518c8da2149065
Rachid Bouzalmat <rbouzalmat@vxcompany.com>
2020-09-21 18:02:19 +0200

    Update;

---
+++ src/stories/mylistitems.stories.ts
@@ -0,0 +1,6 @@
+import {MyListItemsComponent} from '../app/my-list-items/my-list-items.component'
+
+export default {
+    title: "story book demo/Vehicle List",
+    component: MyListItemsComponent
+}
